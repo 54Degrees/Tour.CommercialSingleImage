4505e15e3dbab0affe9668244ee26e17f267ef54
Ellen Corcoran-Walsh <54degreesdesign@gmail.com>
2017-03-16 13:31:27 +0000

    Commercial Tour SAMPLE (Single Image)

---
+++ data.js
@@ -0,0 +1,227 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "map-0-linengreen",
+      "name": "linen Green",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.9108581615399451,
+        "pitch": 0.07104515452502191,
+        "fov": 1.4123007932398033
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4953798092537305,
+          "pitch": 0.09317778748443928,
+          "rotation": 0,
+          "target": "3-restaurant"
+        },
+        {
+          "yaw": -2.5666882374120377,
+          "pitch": -0.002201252907031659,
+          "rotation": 0,
+          "target": "1-hairdresser"
+        },
+        {
+          "yaw": 0.9916456298671061,
+          "pitch": 0.07401820233963541,
+          "rotation": 0,
+          "target": "2-newsagent"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6329383893165215,
+          "pitch": -0.1495332012413222,
+          "title": "Cafe Bedeck",
+          "text": "<p>Here is a description of the business and its USP.</p><h3><strong>Contact Info:</strong></h3><p>info@businessname.com</p><p>+123 4567 8910</p><h3><strong>Opening Hours:</strong></h3><p>Mon- Thur: 09:00 - 18:00</p><p>Fri - Sat: 09:00 - 19:00</p><p>Sun: 12:00 - 18:00​</p>"
+        },
+        {
+          "yaw": 0.8762016198873752,
+          "pitch": -0.13630615177523353,
+          "title": "The Design Yard",
+          "text": "<p>Here is a description of the business and its USP.</p><h3><strong>Contact Info:</strong></h3><p>info@businessname.com</p><p>+123 4567 8910</p><h3><strong>Opening Hours:</strong></h3><p>Mon- Thur: 09:00 - 18:00</p><p>Fri - Sat: 09:00 - 19:00</p><p>Sun: 12:00 - 18:00​</p>"
+        }
+      ]
+    },
+    {
+      "id": "0-linengreen",
+      "name": "linenGreen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.9108581615399451,
+        "pitch": 0.07104515452502191,
+        "fov": 1.4123007932398033
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4953798092537305,
+          "pitch": 0.09317778748443928,
+          "rotation": 0,
+          "target": "3-restaurant"
+        },
+        {
+          "yaw": -2.5666882374120377,
+          "pitch": -0.002201252907031659,
+          "rotation": 0,
+          "target": "1-hairdresser"
+        },
+        {
+          "yaw": 0.9916456298671061,
+          "pitch": 0.07401820233963541,
+          "rotation": 0,
+          "target": "2-newsagent"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6329383893165215,
+          "pitch": -0.1495332012413222,
+          "title": "Cafe Bedeck",
+          "text": "<p>Here is a description of the business and its USP.</p><h3><strong>Contact Info:</strong></h3><p>info@businessname.com</p><p>+123 4567 8910</p><h3><strong>Opening Hours:</strong></h3><p>Mon- Thur: 09:00 - 18:00</p><p>Fri - Sat: 09:00 - 19:00</p><p>Sun: 12:00 - 18:00​</p>"
+        },
+        {
+          "yaw": 0.8762016198873752,
+          "pitch": -0.13630615177523353,
+          "title": "The Design Yard",
+          "text": "<p>Here is a description of the business and its USP.</p><h3><strong>Contact Info:</strong></h3><p>info@businessname.com</p><p>+123 4567 8910</p><h3><strong>Opening Hours:</strong></h3><p>Mon- Thur: 09:00 - 18:00</p><p>Fri - Sat: 09:00 - 19:00</p><p>Sun: 12:00 - 18:00​</p>"
+        }
+      ]
+    },
+    {
+      "id": "1-hairdresser",
+      "name": "Hairdresser",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "yaw": 0.5405623523210732,
+        "pitch": 0.5190856725940591,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33890778751223216,
+          "pitch": 0.1572210759174908,
+          "rotation": 0,
+          "target": "0-linengreen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-newsagent",
+      "name": "Newsagent",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "yaw": 0.4279018932463021,
+        "pitch": 0.16588821867027548,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05336947909254164,
+          "pitch": 0.3865156087878283,
+          "rotation": 0,
+          "target": "0-linengreen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-restaurant",
+      "name": "Restaurant",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 283.5,
+      "initialViewParameters": {
+        "yaw": 2.038855604410844,
+        "pitch": 0.20519439938570017,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0115542079167525,
+          "pitch": 0.4466370425833315,
+          "rotation": 0,
+          "target": "0-linengreen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "The Linen Green",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
